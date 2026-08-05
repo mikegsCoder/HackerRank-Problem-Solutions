50fccfa1ebb2eb13b71fe5c058d432e5bcee0014
mikegsCoder <mikegsid@gmail.com>
2026-08-05 08:45:09 +0300

    Add Sock Merchant problem solution

---
+++ 02. Sock-Merchant/SockMerchant.cs
@@ -0,0 +1,39 @@
+﻿class Solution
+{
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar)
+    {
+        var temp = new List<int>();
+        var pairsCount = 0;
+
+        foreach (var num in ar)
+        {
+            if (temp.Contains(num))
+            {
+                temp.Remove(num);
+                pairsCount++;
+            }
+            else
+            {
+                temp.Add(num);
+            }
+        }
+
+        return pairsCount;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+        int result = sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
