e49b4b720065385fad011ab647603e0339d5f88f
mikegsCoder <mikegsid@gmail.com>
2026-08-08 07:46:19 +0300

    Add mew problem solution

---
+++ 05. Repeated-String/RepeatedString.cs
@@ -0,0 +1,35 @@
+﻿// Repeated String
+class Solution
+{
+    // Complete the repeatedString function below.
+    static long repeatedString(string s, long n)
+    {
+        var fullRepeatCount = n / s.Length;
+
+        var countOriginalText = GetRepeatCount(s);
+
+        int temp = Convert.ToInt32(n - fullRepeatCount * s.Length);
+
+        var countInPartial = GetRepeatCount(s.Substring(0, temp));
+
+        return countOriginalText * fullRepeatCount + countInPartial;
+    }
+
+    static int GetRepeatCount(string s) => s.ToCharArray().Count(x => x == 'a');
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        long n = Convert.ToInt64(Console.ReadLine());
+
+        long result = repeatedString(s, n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
