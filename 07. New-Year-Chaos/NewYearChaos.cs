8184ce13c851bee4b30dddc53a027b5162acec29
mikegsCoder <mikegsid@gmail.com>
2026-08-10 08:43:12 +0300

    Add new problem solution

---
+++ 07. New-Year-Chaos/NewYearChaos.cs
@@ -0,0 +1,36 @@
+﻿// New Year Chaos
+class Solution
+{
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q)
+    {
+        int result = 0;
+
+        for (var i = q.Length - 1; i >= 0; i--)
+        {
+            if (q[i] - (i + 1) > 2)
+            {
+                Console.WriteLine("Too chaotic");
+                return;
+            }
+
+            for (int j = Math.Max(0, q[i] - 2); j < i; j++)
+                if (q[j] > q[i]) result++;
+        }
+
+        Console.WriteLine(result);
+    }
+
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp));
+            minimumBribes(q);
+        }
+    }
+}
