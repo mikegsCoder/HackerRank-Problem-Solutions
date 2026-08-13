cc437e61ff0347cae6a2d4289df87ee88fcee5c0
mikegsCoder <mikegsid@gmail.com>
2026-08-13 08:07:47 +0300

    Add new problem solution

---
+++ 10. Two-Strings/TwoStrings.cs
@@ -0,0 +1,38 @@
+﻿// Two Strings
+class Solution
+{
+    // Complete the twoStrings function below.
+    static string twoStrings(string s1, string s2)
+    {
+        var chars1 = s1.ToCharArray().Distinct().OrderBy(x => x).ToArray();
+
+        foreach (var ch in s2.ToCharArray())
+        {
+            if (Array.BinarySearch(chars1, ch) >= 0)
+                return "YES";
+        }
+
+        return "NO";
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s1 = Console.ReadLine();
+
+            string s2 = Console.ReadLine();
+
+            string result = twoStrings(s1, s2);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
