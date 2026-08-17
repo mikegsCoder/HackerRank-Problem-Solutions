e1b8d391377892f5994d7ff3ee38ec77ef931b41
mikegsCoder <mikegsid@gmail.com>
2026-08-17 07:07:13 +0300

    Add new problem solution

---
+++ 13. Sorting-Bubble-Sort/SortingBubbleSort.cs
@@ -0,0 +1,39 @@
+﻿// Sorting: Bubble Sort
+class Solution
+{
+    // Complete the countSwaps function below.
+    static void countSwaps(int[] a)
+    {
+        if (a.Length == 0)
+            throw new ArgumentException();
+
+        var counter = 0;
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            for (int j = 0; j < a.Length - 1 - i; j++)
+            {
+                if (a[j] > a[j + 1])
+                {
+                    var temp = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = temp;
+
+                    counter++;
+                }
+            }
+        }
+
+        Console.WriteLine($"Array is sorted in {counter} swaps.");
+        Console.WriteLine($"First Element: {a[0]}");
+        Console.WriteLine($"Last Element: {a[a.Length - 1]}");
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp));
+        countSwaps(a);
+    }
+}
