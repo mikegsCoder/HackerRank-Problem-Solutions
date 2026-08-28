45532e32e5dbcd0f5b099d1ee1a50e7e5d6f1530
mikegsCoder <mikegsid@gmail.com>
2026-08-28 07:02:36 +0300

    Add new problem solution

---
+++ 23. Alternating-Characters/AlternatingCharacters.cs
@@ -0,0 +1,39 @@
+﻿// Alternating Characters 
+class Solution
+{
+    // Complete the alternatingCharacters function below.
+    static int alternatingCharacters(string s)
+    {
+        var chr = 'z';
+        var counter = 0;
+
+        for (var index = 0; index < s.Length; index++)
+        {
+            if (s[index] == chr)
+                counter++;
+
+            chr = s[index];
+        }
+
+        return counter;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            int result = alternatingCharacters(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
